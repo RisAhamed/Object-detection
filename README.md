045a0e576916b10711ff05f667dcaa6ac6fbada3
Riswan Ahamed <riswanahamed38@gmail.com>
2024-08-26 13:49:06 +0530

    Template and File structure Created

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Object-detection
+
+# create Environments
+
+Create a separate Environment to install and use the dependencies
+
+```bash 
+conda create -n sign python=3.7 -y ```
+
+``` bash 
+pip install requirements.txt```
+
+
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'signlanguage',
+    version= '0.0.0',
+    author= 'RisAhamed',
+    author_email= 'riswanahamed38@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
